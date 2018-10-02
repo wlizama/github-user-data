5f50c6a1c5f10af67e747d47e46affce819b3cf6
Wilder Humberto Lizama Garcia <wilderlizama@gmail.com>
2018-10-02 10:57:02 -0500

    proyect-restruc - inicio de webpack & vuejs re-estructuracion

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>mi GitHub User Search</div>
+</template>
+
+<script>
+    export default {
+        name : "mainApp"
+    }
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+var path = require('path')
+var webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js', // .[hash:8]
+        publicPath: path.resolve(__dirname, 'dist') + "/",
+        chunkFilename: 'js/[id].js', // .[chunkhash:8]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            // this will apply to both plain `.js` files
+            // AND `<script>` blocks in `.vue` files
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                // reconocer bloques <style> y archivos .css para estraerlos en uno solo
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            minimize: true,
+                        }
+                    }]
+                })
+            },
+            {
+                // loader para imagenes
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'images/'
+                }
+            }
+        ]
+    },
+    devServer: {
+        port: 3179
+    },
+    performance: {
+        hints: false
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new ExtractTextPlugin("css/[name].css"), // .[hash:8].min
+        new OptimizeCssAssetsPlugin({
+            cssProcessorPluginOptions: {
+                preset: ['default', { discardComments: { removeAll: true } }],
+            },
+        }),
+        new CleanWebpackPlugin(['dist'], {root: __dirname})
+    ]
+}
