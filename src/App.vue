37fe23ec223281dc0bb0322dfda4ecf335fcb1d8
Wilder Humberto Lizama Garcia <wilderlizama@gmail.com>
2018-10-02 17:55:56 -0500

    init components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,43 @@
 <template>
-    <div>mi GitHub User Search</div>
+    <section class="main">
+        <InputSearch />
+    </section>
 </template>
 
 <script>
+    import "./assets/styles/common.css"
+    import 'materialize-css/dist/css/materialize.min.css';
+
+    // import ghAPI from "./api"
+
+    import InputSearch from "./components/InputSearch.vue"
+
     export default {
-        name : "mainApp"
+        name : "mainApp",
+        methods : {
+            // micl() {
+            //     ghAPI.getUser("wlizama", (err, data) => {
+            //         if (!err)
+            //             console.log(data);
+            //         else
+            //             console.log(err);
+            //     })
+            // }
+        },
+        components : {
+            InputSearch
+        }
     }
 </script>
+
+<style>
+    .main {
+        min-height: 100%;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position: relative;
+        max-width: 50em;
+    }
+</style>
---
+++ src/components/InputSearch.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="main-search navbar-fixed">
+        <div class="nav-wrapper cyan darken-3">
+            <form autocomplete="off">
+                <div class="input-field">
+                    <input type="search" required placeholder="GitHub user here..." />
+                    <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                    <i class="material-icons">close</i>
+                </div>
+            </form>
+        </div>
+    </nav>    
+</template>
+
+<script>
+    export default {
+        name : "inputSearch"
+    }
+</script>
+
+<style>
+    .main-search{
+        max-width: inherit;
+        display: inline-block;
+    }
+</style>
