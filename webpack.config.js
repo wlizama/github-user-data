acccd88e5bbf2c9c2b2b41936260291c2ef1a123
Wilder Humberto Lizama Garcia <wilderlizama@gmail.com>
2018-10-12 17:57:05 -0500

    temp solution

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/[name].js', // .[hash:8]
-        // publicPath: "dist/",
+        publicPath: '/dist/',
     },
     module: {
         rules: [
@@ -33,8 +33,10 @@ module.exports = {
                         loader: 'css-loader',
                         options: {
                             minimize: true,
+                            sourceMap: true
                         }
-                    }]
+                    }],
+                    publicPath: "../"
                 })
             },
             {
